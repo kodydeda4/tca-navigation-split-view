2e2ac276742543ccef101abf008cd25dba9fbe76
Kody Deda <kodydeda4@gmail.com>
2023-08-10 13:28:40 -0400

    Initial Commit

---
+++ TCA-NavigationSplitView/TCA_NavigationSplitViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_NavigationSplitViewApp.swift
+//  TCA-NavigationSplitView
+//
+//  Created by Kody Deda on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TCA_NavigationSplitViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
